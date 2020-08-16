4ac83bde0239b50eb9a57f7402e2ee29d3fd73b4
Tyler Alsop <tylerjalsop@gmail.com>
2020-08-15 20:06:02 -0600

    Finished ex4.py. Tests passing.

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,21 @@ def has_negatives(a):
     """
     # Your code here
 
+    result = []
+    positive_nums = {}
+
+    for num in a:
+        if num >= 0:
+            if num not in positive_nums:
+                positive_nums[num] = None
+
+    for num in a:
+        if num < 0:
+            neg_to_pos = (num * (-1))
+            if neg_to_pos in positive_nums:
+                if neg_to_pos not in result:
+                    result.append(neg_to_pos)
+
     return result
 
 
